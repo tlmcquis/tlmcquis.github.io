e9327e80ba3c5f4210a0ff33084e213c1493f632
Tommy McQuiston <145158264+tlmcquis@users.noreply.github.com>
2024-12-08 15:35:15 -0500

    Update portfolio.js
    deleted line 95

--- src/portfolio.js
+++ src/portfolio.js
@@ -92,7 +92,6 @@ const educationInfo = {
       logo: require("./assets/images/mtulogo.png"),
       subHeader: "Bachelors of Science in Data Science",
       duration: "September 2021 - April 2026"
-      
     }
   ]
 };
